55fe6c506aa09a7b2cd2aaa4d47162bacf7b96e1
fakhrads <fakhriadisaputra15@gmail.com>
2020-03-13 21:56:15 +0700

    first commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Profile</h1>
+@stop
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            halaman profile
+        </div>
+    </div>
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
